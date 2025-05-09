a08964cf906885f15d6acae0c9c7bdcfe78ecd47
Seungmin-Yang <mse9719@gmail.com>
2025-05-09 13:47:03 +0900

    기능추가:시간에 맞춰서 timer바 내려가게끔 추가
    기능추가:시간에 맞춰서 timer바 내려가게끔 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }        //싱글톤 인스턴스 변수
+    
 
-    //아직 미정
+    //인스펙터에서 가져올 수 있도록 시리얼라이즈필드 설정
     [SerializeField] private PlayerState _player;       //게임오버를 위한 player 가져옴
+    [SerializeField] private UnityEngine.UI.Slider survivalTimeSlider;          //살아남는 시간 표현 슬라이더
 
 
 
-    [SerializeField] private float _stageTimer = 0;    //시간 변수
+
+    //[SerializeField] private float _stageTimer = 0;    //시간 변수
     [SerializeField] private bool _isGameOver = false;     //게임오버 확인 불리언
     [SerializeField] private bool _isStageClear = false;   //스테이지 클리어 확인 불리언
     [SerializeField] private int _score = 0;    //점수 변수
@@ -22,8 +26,10 @@ public class GameManager : MonoBehaviour
     }
     [SerializeField] private int _currentStage = 1;        //스테이지 번호
     public int currentStage { get { return _currentStage; } }
-    [SerializeField] private float _requiredSurvivalTime = 30f;   //버텨야 하는 시간 default
+    [SerializeField] private float _baseSurvivalTime = 30f;     //버텨야 하는 시간 default
+    [SerializeField] private float _requiredSurvivalTime = 0f;
     [SerializeField] private float _increaseDuration = 10f;  //스테이지 거듭할 수록 늘어날 시간 증가값
+    [SerializeField] private int _increaseSpeed = 1;   //스테이지 거듭할 수록 스피드 증가값
 
 
 
@@ -40,24 +46,36 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
 
-        
+        survivalTimeSlider.minValue = 0;
+        survivalTimeSlider.maxValue = _requiredSurvivalTime;
+        survivalTimeSlider.value = _requiredSurvivalTime;
+
+
     }
 
     private void Update()
     {
+
+
+        if (_isStageClear && Input.GetKeyDown(KeyCode.Space))
+        {
+            StageStart();
+        }
         //게임오버나 스테이지 클리어가 true면 아무것도 하지 않고 리턴
         if (_isGameOver || _isStageClear)
         {
             return;
         }
-        _stageTimer += Time.deltaTime;      //스테이지가 지속될때 deltatime으로 더해줌
+        _requiredSurvivalTime = Mathf.Max(0f, _requiredSurvivalTime - Time.deltaTime);     //버텨야 되는 시간을 deltaTime만큼 뺌(0보다 낮은 수가 나오지 않게 하게 최저값을 0으로 지정)
+        survivalTimeSlider.value = _requiredSurvivalTime;
 
         //Debug.Log(_time);
 
-        //스테이지타이머가 버텨야하는 시간보다 높아지면 스테이지 클리어
-        if (_stageTimer >= _requiredSurvivalTime)  
+        //버텨야 하는 시간이 0이 되면 클리어
+        if (_requiredSurvivalTime <= 0)  
         {
             StageClear();
+            return;
         }
 
         //아직 미정
@@ -65,16 +83,22 @@ public class GameManager : MonoBehaviour
         if (_player.CurrentHealth <= 0)
         {
             GameOver();
+            return;
         }
-
     }
 
     //스테이지 시작시 초기화
-    public void StageStart()
+    private void StageStart()
     {
-        _isStageClear = false;
+        _requiredSurvivalTime = _baseSurvivalTime + (_increaseDuration * (_currentStage - 1));      //스테이지마다 버텨야하는 시간값을 갱신
+
+        survivalTimeSlider.minValue = 0;
+        survivalTimeSlider.maxValue = _requiredSurvivalTime;
+        survivalTimeSlider.value = _requiredSurvivalTime;
+
+        //불값 초기화
+        _isStageClear = false;      
         _isGameOver = false;
-        _stageTimer = 0;
     }
 
 
@@ -93,8 +117,11 @@ public class GameManager : MonoBehaviour
     {
         _isStageClear = true;
         Debug.Log("StageClear");
-        _currentStage += 1;
-        _requiredSurvivalTime += _increaseDuration;
+        _currentStage += 1;             //스테이지 ++
+
+        _player.PlayerSpeed += _increaseSpeed * _currentStage;              //플레이어 속도는 증가값 * 스테이지(추후에 변경해야 될 사항)
+
+
     }
 
 
@@ -109,16 +136,20 @@ public class GameManager : MonoBehaviour
     {
         _isStageClear = false;
         _isGameOver = false;
-        _stageTimer = 0f;
-        _currentStage = 0;
+        _currentStage = 1;
         _requiredSurvivalTime = 0f;
         _score = 0;
+        _player.PlayerSpeed = 3;
     }
 
+    public float GetCurrentGameSpeed()
+    {
+        return 3f; // 현재는 임시값, 추후 게임 진행에 따라 증가하도록 변경 가능
+    }
 
 
 
     //TODO:게임오버 조건 만들기 , UI만들고 연결하기(UI매니저로 실시)
     //player스크립트 연결 후([serializeField]로 변수를 만든 후 인스펙터에서 직접 연결
-   
+
 }
