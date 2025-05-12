aad43c7c7b4dc243d9cdf9ee4c97c6ede34f577a
Seungmin-Yang <mse9719@gmail.com>
2025-05-12 21:47:52 +0900

    주석 추가 및 수정:healthUI 주석 추가 및 수정
    주석 추가 및 수정:healthUI 주석 추가 및 수정

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -5,58 +5,53 @@ using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
-    [SerializeField] private Image[] heartImages;
-    [SerializeField] private Sprite fullHeart;
-    [SerializeField] private Sprite emptyHeart;
-    [SerializeField] private Animator animator;
+    [SerializeField] private Image[] heartImages;       //총 3개의 하트이미지를 인스펙터에서 할당함
+    [SerializeField] private Sprite fullHeart;      //꽉찬 하트
+    [SerializeField] private Sprite emptyHeart;         		//빈 하트
+    [SerializeField] private Animator animator; 	//애니메이터
 
-    private int maxHealth;
+    private int maxHealth;              //hp 변수
 
-
+    //체력 초기화 메서드
     public void SetMaxHealth(int hp)
     {
         maxHealth = hp;
         UpdateHealtDisplay(hp);
     }
 
+    //체력 업데이트(실시간 조정) 메서드
     public void UpdateHealtDisplay(int currentHP)
     {
-        for(int i = 0; i < heartImages.Length; i++)
+        //하트 이미지 수 만큼 반복
+        for (int i = 0; i < heartImages.Length; i++)
         {
+            // 애니메이터 할당
             Animator heartAnimator = heartImages[i].GetComponent<Animator>();
+            //i보다 currentHP가 높으면 실행
             if (i<currentHP)
             {
+                //하트이미지[I]번째가 꽉찬하트가 아니면
                 if (heartImages[i].sprite != fullHeart)
                 {
-                    heartImages[i].sprite = fullHeart;
-                    heartAnimator.ResetTrigger("IsHeal");
-                    heartAnimator.SetTrigger("IsHeal");
+                    heartImages[i].sprite = fullHeart;  //그 하트를 꽉찬하트로 바꿔줌
+                    heartAnimator.ResetTrigger("IsHeal");   //HEAL 애니메이션 초기화
+                    heartAnimator.SetTrigger("IsHeal");     //HEAL 애니메이션 출력
                 }
             }
+            //I보다 currentHP가 낮으면 실행
             else
             {
-                if(heartImages[i].sprite != emptyHeart)
+                if(heartImages[i].sprite != emptyHeart)     //I번째 이미지가 빈하트가 아니면
                 {
-                    if(heartAnimator != null)
-                    {
-                        heartImages[i].sprite = emptyHeart;
+                    if(heartAnimator != null)           //애니메이션이 NULL이 아니면
+                    {   
+                        heartImages[i].sprite = emptyHeart;     //꽉찬하트를 빈 하트로 만듦
+                        //피격 애니메이션 실행
                         heartAnimator.ResetTrigger("IsHit");
                         heartAnimator.SetTrigger("IsHit");
                     }
                 }
             }
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
