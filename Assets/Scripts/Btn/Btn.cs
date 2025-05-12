a41d1f610cc61562922030d6c9943b975e6e9b73
whomery <whomery12345@gmail.com>
2025-05-12 10:05:24 +0900

    UI
    UI

--- Assets/Scripts/Btn/Btn.cs
+++ Assets/Scripts/Btn/Btn.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class Btn : MonoBehaviour
 {
     private void OnMouseDown()
     {
-        Debug.Log("스타트 버튼 클릭됨!");
-
-        // 예시 동작: 콘솔 출력, 씬 전환 등
-        // SceneManager.LoadScene("GameScene"); // 게임 씬으로 이동할 경우
+        SceneManager.LoadScene("StageScene");
     }
 }
